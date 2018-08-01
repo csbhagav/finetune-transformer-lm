9072f48a160b15a484d66d883e3b9669d7aaf74a
chandrab <chandrab@allenai.org>
2018-08-01 15:52:36 -0700

    Modify rocstories datareader to load abductive classification data

--- datasets.py
+++ datasets.py
@@ -70,8 +70,8 @@ def _abd_rocstories(path):
         return st, ct1, ct2, y
 
 def abd_rocstories(data_dir, n_train=1497, n_valid=374):
-    storys, comps1, comps2, ys = _rocstories(os.path.join(data_dir, 'train.csv'))
-    teX1, teX2, teX3, _ = _rocstories(os.path.join(data_dir, 'test.csv'))
+    storys, comps1, comps2, ys = _abd_rocstories(os.path.join(data_dir, 'train.csv'))
+    teX1, teX2, teX3, _ = _abd_rocstories(os.path.join(data_dir, 'test.csv'))
     tr_storys, va_storys, tr_comps1, va_comps1, tr_comps2, va_comps2, tr_ys, va_ys = train_test_split(storys, comps1, comps2, ys, test_size=n_valid, random_state=seed)
     trX1, trX2, trX3 = [], [], []
     trY = []
