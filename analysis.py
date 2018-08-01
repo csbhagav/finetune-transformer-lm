ba38592bbf4d2546369e9216cb2f73da190fb698
chandrab <chandrab@allenai.org>
2018-08-01 16:16:05 -0700

    Fix abd analysis

--- analysis.py
+++ analysis.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 from sklearn.metrics import accuracy_score
 
-from datasets import _rocstories
+from datasets import _rocstories, _abd_rocstories
+
 
 def rocstories(data_dir, pred_path, log_path):
     preds = pd.read_csv(pred_path, delimiter='\t')['prediction'].values.tolist()
@@ -20,7 +21,7 @@ def rocstories(data_dir, pred_path, log_path):
 
 def abd_rocstories(data_dir, pred_path, log_path):
     preds = pd.read_csv(pred_path, delimiter='\t')['prediction'].values.tolist()
-    _, _, _, labels = _rocstories(os.path.join(data_dir, 'test.csv'))
+    _, _, _, labels = _abd_rocstories(os.path.join(data_dir, 'test.csv'))
     test_accuracy = accuracy_score(labels, preds)*100.
     logs = [json.loads(line) for line in open(log_path)][1:]
     best_validation_index = np.argmax([log['va_acc'] for log in logs])
